5338abff03de33cab7dd31cc5c1f4b2a1232c73b
Aly Lotfy <95508576+AlyIbrahim1@users.noreply.github.com>
2026-09-22 21:06:13 +0300

    refactor: place the tokens and bundle css in the src file

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '../design-system/tokens.css'
-import '../design-system/components/bundle.css'
+import './styles/tokens.css'
+import './styles/bundle.css'
 import './styles/app.css'
 import { App } from './App'
 
